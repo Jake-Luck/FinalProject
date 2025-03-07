8b9694ae5a3ebcccd686884dd8f5fa02c64f9734
Jake-Luck <47366936+Jake-Luck@users.noreply.github.com>
2025-03-07 17:25:33 +0000

    Removes previous implementation

--- algorithms/branch_and_bound.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from numpy import ndarray  # For type hints
-
-
-
-def branch_and_bound(num_locations: int,
-                     num_days: int,
-                     graph: ndarray) -> ndarray:
-    pass
